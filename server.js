869a264579cd17c2a67aea7447fcbcbef3f0b8b2
Varun Chhabra <varunchhabra0725@gmail.com>
2023-01-19 00:25:48 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Varun Chhabra Student ID: 170279210 Date: 18 Jan 2023
+*
+*  Cyclic Web App URL: ______________________________________________________
+*
+*  GitHub Repository URL: _____________________________________________________
+*
+********************************************************************************/ 
+
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
+
+// setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+    res.send("Varun Chhabra - 170279210");
+});
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT);
